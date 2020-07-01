386c6312923d53d910aabf1d1ffc8ff9d4370519
powerOFMAX <max_maxi@live.com.ar>
2020-07-01 17:40:27 -0300

    Updates

---
+++ precache-manifest.a918d278b64b8098bbea91048dc18f71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5c5bd21c52eaac08475064f62c3fe4",
+    "url": "/maxivega/index.html"
+  },
+  {
+    "revision": "5d34e5776d47a6c6602b",
+    "url": "/maxivega/static/js/2.2c32d83b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/maxivega/static/js/2.2c32d83b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "233ded81240de0909e3e",
+    "url": "/maxivega/static/js/main.2096021c.chunk.js"
+  },
+  {
+    "revision": "3fbb4cae8d88736adf66",
+    "url": "/maxivega/static/js/runtime-main.6ecf1ea2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0da11d69fbca1a6be6b699b2717e1562.js"
+  "/maxivega/precache-manifest.a918d278b64b8098bbea91048dc18f71.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/maxivega/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
